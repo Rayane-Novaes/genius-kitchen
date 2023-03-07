7587036aaea60a9e2d8c6db792cd2fc5ff33e30b
José Victor Vieira <contato.josevictorvieira@gmail.com>
2023-03-06 22:49:57 -0300

    Cabeçalho parcialmente finalizado, com ajuda do Matheus

---
+++ Front/src/Pages/HomePage/HomePage.js
@@ -0,0 +1,30 @@
+import './HomePage.css';
+import '../../assets/common/site.css'
+import Logo from "../../assets/images/Logo.png"
+export default function HomePage() {
+
+  return (
+    <div className='home-page'>
+        <div className="cabecalho">
+            <div className="container">
+                <div className="container-navbar-login">
+                    <div className="btn-login">
+                        <p>Login</p>
+                    </div>
+                </div>
+                <div className="container-navbar-logo">
+                    <img id="navbar-logo" src={Logo}/>
+                    <div className="navbar-logo-titulos">
+                        <p id="kitchen">Kitchen</p>
+                        <h2 id="genius">Genius</h2>
+                    </div>
+                </div>
+                <div className="container-navbar-texto">
+                    <h1>A COMIDA DOS <span>INTELIGENTES</span></h1>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
