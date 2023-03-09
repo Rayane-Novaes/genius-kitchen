765bb04c5ac8473ca23edde12e29625bcf3925b0
José Victor Vieira <contato.josevictorvieira@gmail.com>
2023-03-09 04:59:42 -0300

    Tela parcialmente finalizada.

---
+++ Front/README.md
@@ -0,0 +1,15 @@
+# Projeto Genius Kitchen🍽️
+
+O projeto Genius Kitchen foi desenvolvido como atividade da instituição Centro Universitário Senac - Santo Amaro, sendo voltado a disciplina PI3.  O objetivo é realizar a criação de uma plataforma de um restaunante fictício, com o intuito de utilizar os conhecimentos passados em aula.
+
+## Tecnologias
+![REACT](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)
+![NodeJS](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)
+![MYSQL](https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white)
+![REACT NATIVE](https://img.shields.io/badge/React_Native-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)
+
+## Desenvolvedores :octocat:
+
+| [<img src="https://avatars.githubusercontent.com/u/91327153?v=4" width=115><br><sub>Vinicius Souza</sub>](https://github.com/Vinicius-Souza-Araujo)| [<img src="https://avatars.githubusercontent.com/u/88513836?v=4" width=115><br><sub>Rayane Novaes</sub>](https://github.com/Rayane-Novaes)| [<img src="https://avatars.githubusercontent.com/u/83046050?v=4" width=115><br><sub>Pamera</sub>](https://github.com/Pamera-png?tab=followers) |  [<img src="https://avatars.githubusercontent.com/u/59041432?v=4" width=115><br><sub>José V. Vieira</sub>](https://github.com/jvieira562) | [<img src="https://avatars.githubusercontent.com/u/104471274?v=4" width=115><br><sub>Luan Figueredo</sub>](https://github.com/LuanFigueredo) | [<img src="https://avatars.githubusercontent.com/u/101300600?v=4" width=115><br><sub>Matheus Melo</sub>](https://github.com/MatheusEBMelo) |
+| :---: | :---: | :---: | :---: | :---: | :---:
+
---
+++ Front/src/Pages/HomePage/HomePage.js
@@ -0,0 +1,80 @@
+import './HomePage.css';
+import '../../assets/common/site.css'
+import ItemCardapio from './components/ItemCardapio';
+
+
+
+export default function HomePage() {
+
+    return (
+        <div className='home-page'>
+            <div className="cabecalho">
+                <div >
+                    <div className="container">
+                        <div className="container-navbar-login">
+                            <div className="btn-login">
+                                <a href="#">Login</a>
+                            </div>
+                        </div>
+                        <div className="container-navbar-logo">
+                            <img id="navbar-logo" src="../../../assets/logo.svg" />
+                        </div>
+                        <div className="container-navbar-texto">
+                            <h1>A COMIDA DOS <span>INTELIGENTES</span></h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="secao-2">
+                {/* <img className="img-secao-2" src="./assets/promocoes.svg" /> */}
+                <div class="s2-textos">
+                    <h1><span>PROMOÇÃO</span> COMBO FAMILIA</h1>
+                    <p>Nas compras dos combos familia, consiga um <span>desconto de 30% nas sobremesas</span></p>
+                </div>
+            </div>
+            <div className="container-cardapio-home-page">
+                <div className="container-campo-buscar-home-page">
+                    <input id="campo-busca-home-page" placeholder="procure sua comida aqui">
+                    </input>
+                </div>
+                <div className="container-tab-navegation-home-page">
+                    <div className="tab-navegation-home-page">
+                        <teste href="">Lanches</teste>
+                        <teste href="">Sobremesas</teste>
+                        <teste href="">Bebidas</teste>
+                        <teste href="">Adicionais</teste>
+                    </div>
+                </div>
+                <div className='container-itens-home-page'>
+                    <div className='subcontainer-itens-home-page'>
+                        <ItemCardapio titulo="X-Bacon duplo cheedar" preco="R$ 32,90" imgItem='assets/x-bacon.svg' />
+                        <ItemCardapio titulo="X-Salada clássico" preco="R$ 25,50" imgItem='assets/x-salada.svg' />
+                        <ItemCardapio titulo="Duplo burguer" preco="R$ 37,00" imgItem='assets/duplo-burger.svg' />
+                        <ItemCardapio titulo="X-Egg duplo" preco="R$ 52,00" imgItem='assets/x-egge-duplo.svg' />
+                        <ItemCardapio titulo="Tocco Burger" preco="R$ 42,50" imgItem='assets/tocco-burguer.svg' />
+                        <ItemCardapio titulo="X-Tudo duplo cheedar" preco="R$ 77,50" imgItem='assets/x-tudo.svg' />
+                        <ItemCardapio titulo="Black Burguer" preco="R$ 63,90" imgItem='assets/black-burguer.svg' />
+                        <ItemCardapio titulo="X-Bacon duplo cheedar" preco="R$ 32,90" imgItem='assets/x-bacon.svg' />
+                        <ItemCardapio titulo="X-Salada clássico" preco="R$ 25,50" imgItem='assets/x-salada.svg' />
+                        <ItemCardapio titulo="Duplo burguer" preco="R$ 37,00" imgItem='assets/duplo-burger.svg' />
+                        <ItemCardapio titulo="X-Egg duplo" preco="R$ 52,00" imgItem='assets/x-egge-duplo.svg' />
+                        <ItemCardapio titulo="Tocco Burger" preco="R$ 42,50" imgItem='assets/tocco-burguer.svg' />
+                        {/* <ItemCardapio titulo="X-Tudo duplo cheedar" preco="R$ 77,50" imgItem='assets/x-tudo.svg' />
+                        <ItemCardapio titulo="Black Burguer" preco="R$ 63,90" imgItem='assets/black-burguer.svg' /> */}
+
+                    </div>
+                </div>
+            </div>
+            <div className="secao-4">
+                <div class="s4-textos">
+                    <h1>contato.kitchengenius@genius.com</h1>
+                    <p>Powered By - Kitchen Genius</p>
+                    <p>
+                    <img id="logo-footer" src="../../../assets/logo.svg"></img>
+                    </p>
+                    <p>(11) 9 9999-9999</p>
+                </div>
+            </div>
+        </div>
+    )
+}
