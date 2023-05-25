a42c248ec00a152c562ef3ea0a3531221d6bbaad
Luan Figueredo <luanfigueredodasilva@gmail.com>
2023-05-24 21:10:33 -0300

    Select de produtos funcionando e  inserção na tabel itens pedidos com campos qtd e fkProd

---
+++ geniusKitchen/src/main/java/br/com/geniuskitchen/dao/ItensPedidosDAO.java
@@ -0,0 +1,42 @@
+package br.com.geniuskitchen.dao;
+
+import br.com.geniuskitchen.model.ItensPedidos;
+import br.com.geniuskitchen.model.Pedido;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class ItensPedidosDAO {
+
+    public void createItensPedidos( ItensPedidos itensPedidos) {
+
+        String SQL = "INSERT INTO ITENS_PEDIDOS (QUANTIDADE,FK_PRODUTO) VALUES (?, ?)";
+
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa","sa");
+
+            System.out.println("success in database connection");
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            preparedStatement.setInt(1, itensPedidos.getQuantidade());
+            preparedStatement.setInt(2, Integer.parseInt(itensPedidos.getProduto().getId()));
+//            preparedStatement.setInt(3, itensPedidos.getPedido().getId());
+//            preparedStatement.setString(2, itensPedidos.getObservacao());
+
+            preparedStatement.execute();
+
+            System.out.println("success in insert ITENS_PEDIDOS");
+
+            connection.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("fail in database connection");
+
+        }
+
+    }
+}
